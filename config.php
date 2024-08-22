433475a44e8c910ddbf51566dbfbd5d1e907613c
gauravo7 <gaurav@o7services.com>
2024-08-22 09:58:48 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-	$con=mysqli_connect("http://116.206.104.66","damancp_gymuser","Jalandhar@123","damancp_gym");
+	$con=mysqli_connect("https://o7stu.ksesystem.com","damancp_gymuser","Jalandhar@123","damancp_gym");
 	if(mysqli_connect_errno($con)>0)
 	{
 		echo "Error in Connection".mysqli_connect_error($con);
