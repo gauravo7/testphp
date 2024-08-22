2fd618229cdbefb20ae94402e27e21bb33037a35
gauravo7 <gaurav@o7services.com>
2024-08-22 09:47:04 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-	$con=mysqli_connect("localhost","jankistu_jankistudent","janki@2023","jankistu_gym_gauarv","damancp_gym");
+	$con=mysqli_connect("116.206.104.66","damancp_gymuser","Jalandhar@123","damancp_gym");
 	if(!$con)
 	{
 		echo "<script>window.location.assign('login.php?msg=Error while establishing connection with database')</script>";
